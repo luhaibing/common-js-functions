309b7772a79f7f638207665f209d6924344934ae
mercer <luhaibing12@gmail.com>
2022-11-29 17:31:13 +0800

    部分函数修改

--- tools.js
+++ tools.js
@@ -1,21 +1,47 @@
-// 打印
-function print(...args){
+/**
+ * 学习和编写 tampermonkey 脚本时常用的函数
+ */
+
+/**
+ * 打印
+ * @param args
+ */
+function print(...args) {
     console.log(...args);
 }
 
-// 并发池限制
-async function asyncPool(limit,arr,func,args){
+/**
+ * html字符串 转 document 对象
+ * @param value
+ * @returns {Document}
+ */
+function text2document(value) {
+    let doc;
+    try {
+        doc = document.implementation.createHTMLDocument("");
+        doc.documentElement.innerHTML = value;
+    } catch (error) {
+        throw error;
+    }
+    return doc;
+}
+
+/**
+ * 异步池限制并发数
+ * @param limit 并发数
+ * @param arr   数据的数组
+ * @param func  可执行函数
+ * @param args  参数
+ * @returns {Promise<Awaited<unknown>[]>}
+ */
+async function asyncPool(limit, arr, func, ...args) {
     // 所有任务
     const all = [];
     // 预先执行的任务
     const executings = [];
     for (const item of arr) {
-        let promise = Promise.resolve().then(function(){
-            return func(item,args);
-        }).then(function(){
-            return true;
-        }).catch(function(reason){
-            return false;
+        let promise = Promise.resolve().then(function () {
+            return func(item, ...args);
         });
         // 方便最后统一获取结果
         all.push(promise);
@@ -23,9 +49,9 @@ async function asyncPool(limit,arr,func,args){
             // 限制数 大于 数组长度
             continue;
         }
-        let wrap = promise.then(function(v){
-            // 完成后移除
-            executings.splice(executings.indexOf(wrap),1)
+        let wrap = promise.then(function (v) {
+            // 完成后将自身从正在执行的数组中移除
+            executings.splice(executings.indexOf(wrap), 1)
             return v;
         });
         executings.push(wrap);
@@ -41,68 +67,70 @@ async function asyncPool(limit,arr,func,args){
     return Promise.all(all);
 }
 
-// 建立 请求 和 IO
-async function request(href,arg){
+/**
+ * 使用 XHR 创建 网络请求 和 IO
+ * @param href 链接
+ * @param arg  参数
+ * @returns {Promise<unknown>}
+ */
+async function request(href, arg) {
     let {
         // 超时时间
-        timeout , time_out ,
+        timeout, time_out,
         // 重试次数
-        retry_times , retryTimes ,
+        retry_times, retryTimes,
         // 覆盖发送给服务器的头部,强制 text/xml 作为 mime-type
-        overrideMimeType , mime_type ,
-
-        // // 开始
-        // onloadstart,on_start,
-        // // 数据加载中
-        // onprogress,on_progress,
-        // // 请求成功
-        // onload,on_load,on_success,
-        // // 请求被中止
-        // onabort,on_abort,
-        // // 请求错误
-        // onerror,on_error,on_failure,
-        // // 请求超时
-        // ontimeout,on_timeout,
+        overrideMimeType, mime_type,
     } = arg;
 
-    let _retry_times = retry_times || retryTimes ||3 ;
-    let _time_out = timeout || time_out || 10**3*60*1 ;
-    let _mime_type = overrideMimeType || mime_type || undefined ;
+    let _retry_times = retry_times || retryTimes || 3;
+    let _time_out = timeout || time_out || 10 ** 3 * 60 * 1;
+    let _mime_type = overrideMimeType || mime_type || undefined;
 
-    function run(){
-        return new Promise(function(resolve, reject){
+    /**
+     * 发起请求
+     * @returns {Promise<unknown>}
+     */
+    function run() {
+        return new Promise(function (resolve, reject) {
             let start_time = new Date();
-            let on_success = function(response){
-                print("reqTime : " , (new Date() - start_time), " ; " , href);
+            let on_success = function (response) {
+                print("reqTime : ", (new Date() - start_time), " ; ", href);
                 resolve(response);
             };
-            let on_failure = function(reason){
-                print("reqTime : " , (new Date() - start_time), " ; " , href);
+            let on_failure = function (reason) {
+                print("reqTime : ", (new Date() - start_time), " ; ", href);
                 reject(reason);
             };
             let request_body = {
-                url             : href,
-                method          : "GET",
-                time_out        : _time_out,
+                url: href,
+                method: "GET",
+                time_out: _time_out,
                 overrideMimeType: _mime_type,
-                headers         : {
-                    "referrer"      : location.href,
-                    "Cache-Control" : "no-cache",
-                    "Content-Type"  : "text/html;charset=" + document.characterSet,
+                headers: {
+                    "referrer": location.href,
+                    "Cache-Control": "no-cache",
+                    "Content-Type": "text/html;charset=" + document.characterSet,
                 },
-                onload          : on_success,
-                onabort         : on_failure,
-                onerror         : on_failure,
-                ontimeout       : on_failure,
+                onload: on_success,
+                onabort: on_failure,
+                onerror: on_failure,
+                ontimeout: on_failure,
             };
             GM_xmlhttpRequest(request_body);
         })
     }
 
-    function retry(func,times){
+    /**
+     * 失败后重试
+     * @param func
+     * @param times
+     * @returns {Promise<unknown>}
+     */
+    function retry(func, times) {
         let use = times;
-        return new Promise(async function(resolve, reject){
-            while(use--){
+        return new Promise(async function (resolve, reject) {
+            while (use--) {
                 try {
                     let response = await func();
                     resolve(response);
@@ -115,33 +143,23 @@ async function request(href,arg){
             }
         })
     }
-    return retry(run,_retry_times);
+    
+    return retry(run, _retry_times);
 }
 
-async function request_page(href,){
-    function get_element_doc(value){
-        let doc ;
-        try {
-            doc = document.implementation.createHTMLDocument("");
-            doc.documentElement.innerHTML = value;
-        } catch (error) {
-            throw error;
-        }
-        return doc;
-    }
-    return request(href,{
-        overrideMimeType : "text/html;charset=" + document.characterSet,
-
-    }).then(function(response){
-        return get_element_doc(response.responseText)
-    }).then(function(doc){
+async function html(href,) {
+    return request(href, {
+        overrideMimeType: "text/html;charset=" + document.characterSet,
+    }).then(function (response) {
+        return text2document(response.responseText)
+    }).then(function (doc) {
         print(doc);
         return doc;
     });
 }
 
-async function request_blob(href){
-    return request(href,{
-        overrideMimeType : "text/plain; charset=x-user-defined",
+async function stream(href) {
+    return request(href, {
+        overrideMimeType: "text/plain; charset=x-user-defined",
     });
 }
